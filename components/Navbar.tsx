9b6a1c458132c36fccaccf9a4bd1b0e9b86e22ab
Felipe <souzamarquesfelipe@gmail.com>
2021-07-18 19:01:04 -0300

    fixed navbar styling issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <div className="">
       <div
         className="hidden sm:flex sm:items-center sm:content-around sm:justify-center sm:space-x-2 sm:font-thin 
-      bg-black whitespace-nowrap md:justify-end md:space-x-4 md:font-normal md:p-2 lg:text-lg lg:space-x-6"
+      bg-black whitespace-nowrap md:justify-end md:space-x-4 md:font-normal lg:text-lg lg:space-x-6"
       >
         {navbarElements.map((item, i) => {
           return <NavbarItem title={item.title} key={i} link={item.link} />;
--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -5,7 +5,7 @@ export default function NavbarItem({ title, link }) {
 
   return (
     <button
-      className="text-white cursor-pointer focus:border-b-2 focus:font-black hover:border-b-2 hover:font-black p-4 h-14 focus:outline-none"
+      className="text-white cursor-pointer focus:border-b-2 focus:font-black hover:border-b-2 hover:font-black p-4 h-14 md:h-17 focus:outline-none"
       onClick={() => {
         router.push(link);
       }}
