f8cf5ba0d58f4b5d4c6105b27a96212b3d2fddaa
Felipe <souzamarquesfelipe@gmail.com>
2021-11-04 06:46:45 -0300

    improved footer general look and item placement

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,22 +8,17 @@ export default function Footer() {
   const [selectedLanguage, setSelectedLanguage] = useState(0);
 
   return (
-    <div className="flex flex-col sm:flex-row">
-      <div className="bg-black flex justify-center items-top p-6 sm:w-3/5 sm:h-60">
-        <p className="text-white font-black">Kyoka - 2021</p>
-      </div>
-
-      <div className="flex justify-center sm:justify-end items-top bg-black w-full sm:w-2/5 h-60 ">
-        <Select
-          selectedItem={selectedLanguage}
-          setSelectedItem={setSelectedLanguage}
-          items={languages}
-          className="flex w-full justify-center sm:justify-end mx-8 my-6"
-          className2="px-16 font-bold bg-white py-2 text-xl w-full focus:outline-none 
+    <div className="flex flex-col justify-between items-center bg-black whitespace-nowrap h-60">
+      <Select
+        selectedItem={selectedLanguage}
+        setSelectedItem={setSelectedLanguage}
+        items={languages}
+        className="flex w-full justify-center sm:justify-end my-6 sm:px-6"
+        className2="px-8 sm:px-16 font-bold bg-white py-2 text-xl w-full focus:outline-none 
           focus:shadow-outline-blue focus:border-blue-300 relative border shadow-sm 
           border-gray-300 rounded text-gray-800"
-        />
-      </div>
+      />
+      <p className="text-white font-black my-3">Kyoka - 2021</p>
     </div>
   );
 }
