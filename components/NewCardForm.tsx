cd5aac443a4f2e0b2401317ce6feb192cea08416
Felipe <souzamarquesfelipe@gmail.com>
2021-11-17 07:43:10 -0300

    fixed formik issue

--- components/NewCardForm.tsx
+++ components/NewCardForm.tsx
@@ -2,37 +2,21 @@ import { Formik, Form } from "formik";
 import { TextField } from "./TextField";
 import * as Yup from "yup";
 import { TextArea } from "./TextArea";
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ImageDropzone from "./ImageDropzone";
 import AudioDropzone from "./AudioDropzone";
-import axios from "axios";
+
 import { api, verifyToken } from "../utils/api";
 import router from "next/router";
 
 export const NewCardForm = ({ deckId }) => {
   const [fields, setFields] = useState([]);
-  const [initialVal, setInitialVal] = useState([]);
-  // const [currentFieldName, setCurrentFieldName] = useState("");
-  let fieldData = [];
+  const [initialVal, setInitialVal] = useState({});
 
   const [initValues, setInitValues] = useState(false);
   const numberOfImages = Array.from(Array(4).keys()); //4 images
 
-  let labels = [];
-  let init = [];
-
-  labels["sentence"] = "Frase";
-  labels["focus"] = "Foco";
-  labels["bilingualDescription"] = "Descrição bilíngue";
-  labels["monolingualDescription"] = "Descrição monolíngue";
-  labels["translation"] = "Tradução";
-  labels["notes"] = "Observações";
-  labels["sentenceAudio"] = "Áudio da frase";
-  labels["focusAudio"] = "Áudio do foco";
-  labels["images"] = "Imagens";
-
-  /*   fileHolders["sentenceAudio"];
-  fileHolders["focusAudio"]; */
+  let init = {};
 
   useEffect(() => {
     api.get(`decks/${deckId}`).then((res) => {
@@ -52,23 +36,25 @@ export const NewCardForm = ({ deckId }) => {
             switch (item.fieldType) {
               case 0:
               case 1:
-                init[currentField] = ""; //init[currentField] = "hm";
+                init[currentField] = "";
                 break;
 
               case 2:
                 init[currentField] = "";
-                //init[holder] = { name: "" };
+                init[holder] = "";
                 break;
 
               case 3:
                 numberOfImages.map((item2) => {
                   init[currentField + item2.toString()] = "";
-                  //init[holder + item2.toString()] = { name: "" };
+                  init[holder + item2.toString()] = "";
                 });
                 break;
             }
 
             setInitialVal(init);
+
+            console.log(init);
             setInitValues(true);
           });
         });
@@ -88,7 +74,7 @@ export const NewCardForm = ({ deckId }) => {
     });
   }
 
-  function sendToServer() {
+  function sendToServer(values) {
     //Get user ID from jwt token
     const userId = verifyToken();
 
@@ -98,22 +84,19 @@ export const NewCardForm = ({ deckId }) => {
     const config = { headers: { "Content-Type": "multipart/form-data" } };
     let fd = new FormData();
 
-    Object.entries(fieldData).map((item) => {
-      /*       console.log(item[0]);
-      console.log(typeof item[1]); */
+    console.log(values);
 
-      if (item[0].length > 0) {
-        if (typeof item[1] === "object") {
-          const newFile = renameFile(item[1], item[0]);
+    Object.entries(values).map((item: any) => {
+      if (typeof item[1] === "object") {
+        const newFile = renameFile(item[1], item[0]);
 
-          if (item[1].type.split("/")[0] == "image") {
-            imageData.push(newFile);
-          } else if (item[1].type.split("/")[0] == "audio") {
-            audioData.push(newFile);
-          }
-        } else {
-          fd.append(item[0], item[1]);
+        if (item[1].type.split("/")[0] == "image") {
+          imageData.push(newFile);
+        } else if (item[1].type.split("/")[0] == "audio") {
+          audioData.push(newFile);
         }
+      } else if (item[1].length > 0) {
+        fd.append(item[0], item[1]);
       }
     });
 
@@ -134,9 +117,6 @@ export const NewCardForm = ({ deckId }) => {
     api
       .post(`cards/`, fd, config)
       .then((res) => {
-        console.log(res.data);
-      })
-      /*   .then((res) => {
         //Get card ID
         const cardId = res.data._id;
 
@@ -147,80 +127,10 @@ export const NewCardForm = ({ deckId }) => {
             user: userId,
           })
           .then(() => router.back()); //redirect user to the deck page
-      }) */
+      })
       .catch((err) => {
         console.log(err);
       });
-
-    /*  const lastChar = item[0].slice(-1);
-        const isNotNumber = isNaN(parseInt(lastChar));
-
-        if (isNotNumber) {
-          fd.append(item[0], item[1]);
-        } else {
-          if (!arrayFieldData[item[0].slice(0, -1)]) {
-            arrayFieldData[item[0].slice(0, -1)] = [];
-          }
-
-          arrayFieldData[item[0].slice(0, -1)].push(item[1]);
-        } */
-
-    //console.log(audioHolders);
-    //console.log(imageHolders);
-    /*     if (values.sentenceAudioHolder.name !== "") {
-      files["sentenceAudio"] = values.sentenceAudioHolder;
-    }
-
-    if (values.focusAudioHolder.name !== "") {
-      files["focusAudio"] = values.focusAudioHolder;
-    } */
-
-    /*     const images = [
-      values.image1Holder,
-      values.image2Holder,
-      values.image3Holder,
-      values.image4Holder,
-    ];
-
-    files["images"] = images.filter((image) => {
-      return image !== "";
-    });
- */
-
-    /*     files["images"].map((image) => {
-      fd.append("images", image);
-    });
-
-    fd.append("sentence_audio", files["sentenceAudio"]);
-    fd.append("focus_audio", files["focusAudio"]);
-
-    fd.append("deck", deckId);
-    fd.append("sentence", values.sentence);
-    fd.append("focus", values.focus);
-    fd.append("bilingualDescription", values.bilingualDescription);
-    fd.append("monolingualDescription", values.monolingualDescription);
-    fd.append("translation", values.translation);
-    fd.append("notes", values.notes);
- */
-    //fd.append("creator", userId);
-
-    /* api
-      .post(`cards/`, fd, config)
-      .then((res) => {
-        //Get card ID
-        const cardId = res.data._id;
-
-        //Create SRS stats for the card
-        api
-          .post("/card-stats", {
-            card: cardId,
-            user: userId,
-          })
-          .then(() => router.back()); //redirect user to the deck page
-      })
-      .catch((err) => {
-        console.log(err);
-      }); */
   }
 
   return (
@@ -230,193 +140,107 @@ export const NewCardForm = ({ deckId }) => {
           initialValues={initialVal}
           validationSchema={validate}
           enableReinitialize
-          onSubmit={() => sendToServer()}
+          onSubmit={(values) => sendToServer(values)}
         >
-          <Form
-            className="bg-white flex flex-col justify-center items-center sm:my-8 space-y-8 w-full
+          {(formik) => (
+            <Form
+              className="bg-white flex flex-col justify-center items-center sm:my-8 space-y-8 w-full
         sm:shadow-lg sm:rounded-lg sm:w-4/5 sm:items-start sm:justify-start p-4
         whitespace-nowrap"
-            onChange={(e: any) => {
-              fieldData[e.target.name] = e.target.value;
-            }}
-          >
-            <h1 className="font-black text-3xl sm:text-5xl">Registrar carta</h1>
+            >
+              <h1 className="font-black text-3xl sm:text-5xl">
+                Registrar carta
+              </h1>
 
-            <div className="flex flex-col w-full space-y-4">
-              {fields.map((item, i) => {
-                const currentFieldName = item.fieldName;
-                switch (item.fieldType) {
-                  case 0:
-                    return (
-                      <TextField
-                        key={i}
-                        label={labels[currentFieldName]}
-                        name={currentFieldName}
-                        type="text"
-                      />
-                    );
+              <div className="flex flex-col w-full space-y-4">
+                {fields.map((item, i) => {
+                  const currentFieldName = item.fieldName;
+                  const labelNumber = 0;
 
-                  case 1:
-                    return (
-                      <TextArea
-                        key={i}
-                        label={labels[currentFieldName]}
-                        name={currentFieldName}
-                        type="text"
-                      />
-                    );
-
-                  case 2:
-                    return (
-                      <div key={i}>
-                        <AudioDropzone
-                          label={labels[currentFieldName]}
+                  switch (item.fieldType) {
+                    case 0:
+                      return (
+                        <TextField
+                          key={i}
+                          label={item.fieldLabel[labelNumber]}
                           name={currentFieldName}
-                          fileExchange={(audio) => {
-                            /*         formik.setFieldValue(
-                                currentFieldName + "Holder",
-                                audio
-                              ); */
-
-                            fieldData[currentFieldName] = audio;
-                          }}
+                          type="text"
                         />
+                      );
 
-                        {/* <input name={currentFieldName + "Holder"} hidden /> */}
-                      </div>
-                    );
+                    case 1:
+                      return (
+                        <TextArea
+                          key={i}
+                          label={item.fieldLabel[labelNumber]}
+                          name={currentFieldName}
+                          type="text"
+                        />
+                      );
 
-                  case 3:
-                    return (
-                      <div
-                        className="flex flex-wrap flex-col space-y-4 xs:space-x-4 xs:space-y-0 xs:flex-row"
-                        key={i}
-                      >
-                        <div className="flex space-x-4 xs:space-x-4">
-                          {numberOfImages.map((item) => {
-                            return (
-                              <div key={item}>
-                                <ImageDropzone
-                                  name={currentFieldName + item.toString()}
-                                  fileExchange={(image) => {
-                                    /*formik.setFieldValue(
-                                        currentFieldName +
-                                          "Holder" +
-                                          item.toString(),
-                                        image
-                                      ); */
+                    case 2:
+                      return (
+                        <div key={i}>
+                          <AudioDropzone
+                            label={item.fieldLabel[labelNumber]}
+                            name={currentFieldName + "Holder"}
+                            fileExchange={(audio) => {
+                              formik.setFieldValue(currentFieldName, audio);
+                            }}
+                          />
 
-                                    fieldData[
-                                      currentFieldName + item.toString()
-                                    ] = image;
-                                  }}
-                                />
-
-                                {/* <input
-                                    name={
-                                      currentFieldName +
-                                      "Holder" +
-                                      item.toString()
-                                    }
-                                    hidden
-                                  /> */}
-                              </div>
-                            );
-                          })}
+                          <input name={currentFieldName} hidden />
                         </div>
-                      </div>
-                    );
-                }
-              })}
+                      );
 
-              {/*  <TextField label="Frase" name="sentence" type="text" />
-            <TextField label="Foco" name="focus" type="text" />
-            <TextArea
-              label="Descrição bilíngue"
-              name="bilingualDescription"
-              type="text"
-            />
-            <TextArea
-              label="Descrição monolíngue"
-              name="monolingualDescription"
-              type="text"
-            />
+                    case 3:
+                      return (
+                        <>
+                          <label className="text-xl font-normal">
+                            {item.fieldLabel[labelNumber]}
+                          </label>
+                          <div
+                            className="flex flex-wrap flex-col space-y-4 xs:space-x-4 xs:space-y-0 xs:flex-row"
+                            key={i}
+                          >
+                            <div className="flex space-x-4 xs:space-x-4">
+                              {numberOfImages.map((item) => {
+                                return (
+                                  <div key={item}>
+                                    <ImageDropzone
+                                      name={
+                                        currentFieldName +
+                                        "Holder" +
+                                        item.toString()
+                                      }
+                                      fileExchange={(image) => {
+                                        formik.setFieldValue(
+                                          currentFieldName + item.toString(),
+                                          image
+                                        );
+                                      }}
+                                    />
 
-            <AudioDropzone
-              label="Áudio da frase"
-              name="sentenceAudio"
-              fileExchange={(audio) => {
-                formik.setFieldValue("sentenceAudioHolder", audio);
-              }}
-            />
-            <label>{formik.values.sentenceAudioHolder.name}</label>
-
-            <input name="sentenceAudioHolder" hidden />
-            <input name="focusAudioHolder" hidden />
-
-            <AudioDropzone
-              label="Áudio do foco"
-              name="focusAudio"
-              fileExchange={(audio) => {
-                formik.setFieldValue("focusAudioHolder", audio);
-              }}
-            />
-
-            <label>{formik.values.focusAudioHolder.name}</label>
-
-            <label className="font-normal text-xl">Imagens</label>
-
-            <input type="file" name="image1Holder" hidden />
-            <input type="file" name="image2Holder" hidden />
-            <input type="file" name="image3Holder" hidden />
-            <input type="file" name="image4Holder" hidden />
-
-            <div className="flex flex-wrap flex-col space-y-4 xs:space-x-4 xs:space-y-0 xs:flex-row">
-              <div className="flex space-x-4 xs:space-x-4">
-                <ImageDropzone
-                  name="image1"
-                  fileExchange={(image) => {
-                    formik.setFieldValue("image1Holder", image);
-                  }}
-                />
-
-                <ImageDropzone
-                  name="image2"
-                  fileExchange={(image) => {
-                    formik.setFieldValue("image2Holder", image);
-                  }}
-                />
+                                    <input
+                                      name={currentFieldName + item.toString()}
+                                      hidden
+                                    />
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          </div>
+                        </>
+                      );
+                  }
+                })}
               </div>
 
-              <div className="flex space-x-4 xs:space-x-4">
-                <ImageDropzone
-                  name="image3"
-                  fileExchange={(image) => {
-                    formik.setFieldValue("image3Holder", image);
-                  }}
-                />
-                <ImageDropzone
-                  name="image4"
-                  fileExchange={(image) => {
-                    formik.setFieldValue("image4Holder", image);
-                  }}
-                />
-              </div>
-            </div>
-
-            <TextField
-              label="Tradução da frase"
-              name="translation"
-              type="text"
-            />
-
-            <TextArea label="Observações" name="notes" type="text" /> */}
-            </div>
-
-            <button className="confirmation-button" type="submit">
-              Criar
-            </button>
-          </Form>
+              <button className="confirmation-button" type="submit">
+                Criar
+              </button>
+            </Form>
+          )}
         </Formik>
       )}
     </>
