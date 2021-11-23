591d0e4706ebb25ad9e67d6bd3b5a830f62369db
Felipe <souzamarquesfelipe@gmail.com>
2021-11-23 20:33:12 -0300

    added missing translation entry

--- utils/customHooks.tsx
+++ utils/customHooks.tsx
@@ -1,16 +1,19 @@
 import { useMemo } from "react";
 import * as Yup from "yup";
+import { useTranslation } from "next-i18next";
 
 export function useValidator(needValidation) {
+  const { t } = useTranslation();
+
   return useMemo(() => {
     const validationObject: { [key: string]: Yup.StringSchema } = {};
 
     needValidation.forEach((item) => {
       validationObject[item.fieldName] = Yup.string().required(
-        "Este campo é obrigatório"
+        t("field_required")
       );
     });
 
     return Yup.object(validationObject);
-  }, [needValidation]);
+  }, [needValidation, t]);
 }
